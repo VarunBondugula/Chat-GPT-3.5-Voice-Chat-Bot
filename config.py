7b8cbc03519975f95004ffcc454a60ec8743ed8a
VarunBondugula <varunbondugula@gmail.com>
2023-01-01 16:07:46 -0500

    Verification System

---
+++ config.py
@@ -0,0 +1,9 @@
+class Config:
+    #essentials
+    prefix = "!"
+    bot_token = "MTA1OTIwMzI3MDExMjk3Mjg1MA.GSQF9s.2eXXtqFWTYXF_UdCVoc5pbcAkGqj3-vCRm0TGk"
+    
+    #verification
+    role_name = "Member"
+    message_id = "1059202747502710784"
+    channel_id = "1059202446301347841"
